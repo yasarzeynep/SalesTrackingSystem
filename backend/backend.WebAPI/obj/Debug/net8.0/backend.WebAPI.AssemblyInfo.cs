dfc422e7a296af76b998ee4d78311b63d9544d47
Zeynep Yaşar <zynpsema@gmail.com>
2024-07-16 11:48:23 +0300

    invoice, production crud

--- backend/backend.WebAPI/obj/Debug/net8.0/backend.WebAPI.AssemblyInfo.cs
+++ backend/backend.WebAPI/obj/Debug/net8.0/backend.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484a410f40b673571d23e2efb53a7cf6e9579e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720f317b227e816c29dfc1678f9a0f60c06f6203")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
